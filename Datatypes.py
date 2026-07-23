9ec64c1db4d63ef805ca0ede9ed74a25d713849e
Samruddhi3406 <samruddhitalekar2006@gmail.com>
2026-07-23 12:37:35 +0530

    Add Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,31 @@
+#Basic 
+name="samruddhi"
+age=20
+branch="CSE"
+div='A'
+percentage=83.3
+print("Name =",name)
+print(type(name))
+print("Age =",age)
+print(type(age))
+print("Branch =",branch)
+print(type(branch))
+print("Divison =",div)
+print(type(div))
+print("Percentage=",percentage)
+print(type(percentage))
+
+#Advanced
+L=[10,20,30,40,50]
+S={10,"hello",80.8}
+T=(10,34,76,89)
+D={ "Name" :"Samruddhi", "Division" : 'A',"Roll no." : 11}
+print(type(L))
+print(type(S))
+print(type(T))
+print(type(D))
+
+name=input("enter name:")
+rollno=int(input("enter roll no:"))
+print(name)
+print(rollno)
