46d2a2a3f3a2517e65bd81cbce82d7c874083476
Samruddhi3406 <samruddhitalekar2006@gmail.com>
2026-08-20 12:59:31 +0530

    Initial Upload

---
+++ Function.py
@@ -0,0 +1,41 @@
+#1.To add two numbers:
+# def add(x,y):
+#     return x+y
+
+# x=10
+# y=20
+# result=add(x,y)
+# print("The sum of is:",result)
+
+#2.To calculate area of circle:
+# def area(radius):
+#     return 3.14*radius*radius
+
+# radius=float(input("Enter a radius of a circle:"))
+
+# Area=area(radius)
+# print("Area of a circle is:",Area)
+
+#3.To reverse a string
+# def Reverse(String):
+#     return String[::-1]
+
+# String=input("Enter a string:")
+# print("Reverse String is :",Reverse(String))
+
+#4.To Greet
+# def greet(Name):
+#     print("Hello",Name)
+
+# Name=input("Enter your name:")
+# greet(Name)
+
+#5.To check number is odd or even
+# def check(num):
+#     if num%2==0:
+#         print("Number is Even")
+#     else:
+#         print("Number is Odd")
+
+# num=int(input("Enter a number:"))
+# check(num)
